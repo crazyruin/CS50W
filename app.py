1c0050970fbcecf919b172a1d8022e0ccf89fb07
Anders Mygind Jepsen <anders.mygind.jepsen@gmail.com>
2018-12-18 13:34:33 +0100

    Added nav to characters

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
+from flask_session import Session
 
 app = Flask(__name__)
 app.static_folder = 'static'
